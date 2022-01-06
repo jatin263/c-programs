61f046247d5834293d9804b14f59c90bea82e307
Jatin Kumawat <64724543+jatin263@users.noreply.github.com>
2022-01-06 12:45:10 +0530

    Add files via upload

---
+++ Ascii.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	char ch;
+	printf("Enter any Character :- \n");
+	scanf("%c",&ch);
+	printf("The ASCII Value of %c is %d",ch,ch);
+	return 0;
+}
---
+++ sizeof.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int i;
+	float j;
+	char k;
+	double l;
+	printf("The size of int is %d\n",sizeof(i));
+	printf("The size of float is %d\n",sizeof(j));
+	printf("The size of char is %d\n",sizeof(k));
+	printf("The size of double is %d\n",sizeof(l));
+	return 0;
+}
